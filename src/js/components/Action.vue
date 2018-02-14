3b039b22bd0dca19032b061d48f738bf03913ad1
Ben Rowe <ben.rowe.83@gmail.com>
2018-02-15 07:39:02 +1100

    Add action component

---
+++ src/js/components/Action.vue
@@ -0,0 +1,11 @@
+<template>
+  <button type="button" :title="tooltip"><slot></slot></button>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  props: ['tooltip']
+})
+</script>
